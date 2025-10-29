00d4a498dc184b33e9e168078d23ccffb666d528
Georgi Goranov <124670356+GeorgiGoranov@users.noreply.github.com>
2025-10-29 13:58:30 +0100

    save only unique users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,18 +1,28 @@
 const UserModel = require('../models/UserModel')
 
 const getUser = async (req, res) => {
-    res.status(200).json({ message: "coll! _l_" })
+    res.status(200).json({ message: "coll!" })
 }
 
 const setUser = async (req, res) => {
     const { name } = req.body
     const score = req.cookies['trex-high-score'] || "0";
 
-    try{
-        const user = await UserModel.create({name,score})
-        res.status(200).json({user})
-    }catch(error){
-        res.status(400).json({error: error.message})
+    try {
+
+        const user = await UserModel.findOne({ name });
+
+        if (user) {
+            user.score = score;
+            await user.save();
+            return res.status(200).json({ message: "User updated", user });
+        }
+
+        const newUser = await UserModel.create({ name, score })
+        res.status(200).json({ message: "User created", newUser })
+
+    } catch (error) {
+        res.status(400).json({ error: error.message })
     }
 
 }
